108198c2b1bbe02a64bb968f3760168d94120fe2
Carlo Sciolla <carlo.sciolla@gmail.com>
2018-06-08 20:46:32 +0200

    Use pyvenv to do stuff with python

---
+++ lisp/03-python.el
@@ -0,0 +1,24 @@
+;;; 03-python.el --- Part of my Emacs startup configuration  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Carlo Sciolla
+
+;; Author: Carlo Sciolla <skuro@margiani>
+;; Keywords: convenience
+
+;;; Commentary:
+
+;; Python configuration
+
+;;; Code:
+
+(use-package pyvenv
+  :ensure t)
+
+(provide '03-python)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; 03-python.el ends here
