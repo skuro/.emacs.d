4518e3a1ab322ded2dbb04b7fe8f2c037b16c0db
Carlo Sciolla <carlo.sciolla@gmail.com>
2019-11-04 15:24:03 +0100

    New function to kill lines matching regex
    From the Emacs wiki

--- lisp/00-editing.el
+++ lisp/00-editing.el
@@ -797,6 +797,35 @@ Add this to `kill-buffer-query-functions'."
     (when open-cmd
       (start-process "" nil open-cmd file))))
 
+;;;###autoload
+(defun skuro/kill-matching-lines (regexp &optional rstart rend interactive)
+  "Kill lines containing matching REGEXP.
+
+Operates on either the full buffer or within the region delimited
+by RSTART and REND.
+
+Can be interatively executed in which case it's set as INTERACTIVE.
+
+See `flush-lines' or `keep-lines' for behavior of this command.
+
+If the buffer is read-only, Emacs will beep and refrain from deleting
+the line, but put the line in the kill ring anyway.  This means that
+you can use this command to copy text from a read-only buffer.
+\(If the variable `kill-read-only-ok' is non-nil, then this won't
+even beep.)"
+  (interactive
+   (keep-lines-read-args "Kill lines containing match for regexp"))
+  (let ((buffer-file-name nil)) ;; HACK for `clone-buffer'
+    (with-current-buffer (clone-buffer nil nil)
+      (let ((inhibit-read-only t))
+        (keep-lines regexp rstart rend interactive)
+        (kill-region (or rstart (line-beginning-position))
+                     (or rend (point-max))))
+      (kill-buffer)))
+  (unless (and buffer-read-only kill-read-only-ok)
+    ;; Delete lines or make the "Buffer is read-only" error.
+    (flush-lines regexp rstart rend interactive)))
+
 (provide '00-editing)
 
 ;; Local Variables:
